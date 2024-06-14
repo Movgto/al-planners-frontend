d0f704bab7bf9273fd5effa13b87926bbc23b806
Gumaro Monroy <maromvz@gmail.com>
2024-06-14 02:10:35 -0600

    refactor: Remove unused code

--- src/views/admins/CalendarView.tsx
+++ src/views/admins/CalendarView.tsx
@@ -4,24 +4,17 @@ import { CalendarDate, CalendarTab, calendarTabs } from '@/types/index'
 import CalendarTabs from '@/components/admins/calendar/CalendarTabs'
 import { classes } from '@/utils/index'
 import { useLocation } from 'react-router-dom'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import { getAuthURL, getEvents } from '@/api/calendarAPI'
-import { toast } from 'react-toastify'
 
 const CalendarView = () => {
   const location = useLocation()
   const search = new URLSearchParams(location.search)
   const code = search.get('code')
+  const codeExists = !!code
 
   const [date, setDate] = useState<CalendarDate>(new Date())
   const [tab, setTab] = useState<CalendarTab>('Disponibilidad')
-
-  // const {data, isError} = useQuery({
-  //   queryKey: ['authURL'],
-  //   queryFn: getAuthURL
-  // })
-
-  const queryClient = useQueryClient()
 
   const handleTabClick = (tab: CalendarTab) => {
     setTab(tab)
@@ -31,15 +24,15 @@ const CalendarView = () => {
 
   const {data} = useQuery({
     queryKey: ['authURL'],
-    queryFn: getAuthURL
+    queryFn: getAuthURL,
+    refetchOnWindowFocus: false
   })
   
-  const {mutate} = useMutation({
-    mutationFn: getEvents,
-    onError: (error) => toast.error(error.message),
-    onSuccess: (data) => {
-      toast.success(data)
-    }
+  const {data : calendarData} = useQuery({
+    queryKey: ['calendarEvents'],
+    queryFn: () => getEvents(code!),
+    enabled: codeExists,
+    refetchOnWindowFocus: false,
   })
 
   const handleLoginGoogle = () => {
@@ -63,38 +56,43 @@ const CalendarView = () => {
     </div>
   )
 
-  return (
-    <div
-      className="max-w-5xl flex flex-col py-24 gap-2"
-    >
+  if (calendarData) {
+    console.log(calendarData)
+    return (
       <div
-        className='border-b-2 border-slate-300'
+        className="max-w-5xl flex flex-col py-24 gap-2"
       >
         <div
-          className='flex gap-2 -mb-[2px]'
+          className='border-b-2 border-slate-300'
         >
-          {calendarTabs.map(t => (
-            <button
-              key={t}
-              type="button"
-              className={classes(tab === t ? 'border-rose-400 font-bold text-rose-700' : 'border-slate-400 text-slate-400', 'border-b-2 py-2')}
-              onClick={() => handleTabClick(t)}
-            >{t}</button>
-          ))}
-        </div>        
-      </div>
-      <div className='flex flex-col lg:flex-row'>
-        <div
-          className="flex-1 bg-violet-50"
-        >
-          <Calendar date={date} setDate={setDate} code={code} />
+          <div
+            className='flex gap-2 -mb-[2px]'
+          >
+            {calendarTabs.map(t => (
+              <button
+                key={t}
+                type="button"
+                className={classes(tab === t ? 'border-rose-400 font-bold text-rose-700' : 'border-slate-400 text-slate-400', 'border-b-2 py-2')}
+                onClick={() => handleTabClick(t)}
+              >{t}</button>
+            ))}
+          </div>        
         </div>
-        <div className="flex-1 bg-violet-50 py-4">
-          <CalendarTabs tab={tab} />
+        <div className='flex flex-col lg:flex-row'>
+          <div
+            className="flex-1 bg-violet-50"
+          >
+            <Calendar date={date} setDate={setDate} code={code} />
+          </div>
+          <div className="flex-1 bg-violet-50 py-4">
+            <CalendarTabs tab={tab} />
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  return null
 }
 
 export default CalendarView
