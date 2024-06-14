728aea6dabc00a51b0b6580778d97ece44355910
Gumaro Monroy <maromvz@gmail.com>
2024-06-14 02:07:58 -0600

    refactor: Remove unnecesary tab from calendar tabs component

--- src/components/admins/calendar/CalendarTabs.tsx
+++ src/components/admins/calendar/CalendarTabs.tsx
@@ -1,6 +1,5 @@
 import { CalendarTab } from "@/types/index"
 import AvailabilityTab from "./AvailabilityTab"
-import SyncTab from "./SyncTab"
 
 type CalendarTabsProps = {
   tab: CalendarTab,
@@ -8,8 +7,6 @@ type CalendarTabsProps = {
 
 const CalendarTabs = ({tab} : CalendarTabsProps) => {
   if(tab === 'Citas') return 'Citas en construccion'
-
-  if(tab === 'Sincronizar') return <SyncTab />
 
   if(tab === 'Disponibilidad') return <AvailabilityTab />
 
