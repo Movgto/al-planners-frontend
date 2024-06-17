253212172b48ac53a9e8f678722242eda563fb89
Gumaro Monroy <maromvz@gmail.com>
2024-06-17 12:54:04 -0600

    refactor: Update calendarAPI to use consistent naming conventions

--- src/api/calendarAPI.ts
+++ src/api/calendarAPI.ts
@@ -1,4 +1,5 @@
 import ax, {isAxiosError} from 'axios'
+import { EventFormData, EventTypeFormData, eventTypesSchema } from '../types'
 
 const {VITE_API_URL} = import.meta.env
 
@@ -21,11 +22,60 @@ export const getAuthURL = async () => {
 
 }
 
-export const getEvents = async (code: string) => {
+export const getGoogleAPIEvents = async (code: string) => {
   const url = VITE_API_URL + '/api/googleapi/events?code=' + code
 
   try {
     const {data} = await ax(url)
+
+    return data
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const getEvents = async () => {
+  const url = VITE_API_URL + '/api/events'
+
+  try {
+    const {data} = await ax(url)
+     return data
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const getEventTypes = async () => {
+  const url = VITE_API_URL + '/api/eventTypes'
+
+  try {
+    const {data} = await ax(url)
+
+    const result = eventTypesSchema.safeParse(data)
+    console.log(result)
+    if (result.success) return result.data
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const createEvent = async (formData : EventFormData) => {
+  const url = VITE_API_URL + '/api/events'
+
+  try {
+    const {data} = await ax.post(url, formData)
+
+    return data
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const createEventType = async (formData : EventTypeFormData) => {
+  const url = VITE_API_URL + '/eventTypes'
+
+  try {
+    const {data} = await ax.post(url, formData)
 
     return data
   } catch (error) {
