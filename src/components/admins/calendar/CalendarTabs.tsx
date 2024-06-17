9be17bc604bca1b9941be3cfb4a700474056e90c
Gumaro Monroy <maromvz@gmail.com>
2024-06-17 12:54:30 -0600

    refactor: Add AppointmentsTab component for managing appointments in the calendar

--- src/components/admins/calendar/CalendarTabs.tsx
+++ src/components/admins/calendar/CalendarTabs.tsx
@@ -1,12 +1,13 @@
 import { CalendarTab } from "@/types/index"
 import AvailabilityTab from "./AvailabilityTab"
+import AppointmentsTab from "./AppointmentsTab"
 
 type CalendarTabsProps = {
   tab: CalendarTab,
 }
 
 const CalendarTabs = ({tab} : CalendarTabsProps) => {
-  if(tab === 'Citas') return 'Citas en construccion'
+  if(tab === 'Citas') return <AppointmentsTab />
 
   if(tab === 'Disponibilidad') return <AvailabilityTab />
 
