5e91bdf163bea38bbbcc494116675aab2e2d1ff3
Gumaro Monroy <maromvz@gmail.com>
2024-06-17 12:54:48 -0600

    refactor: Update event types and schemas in types/index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,3 +16,33 @@ export const calendarTabs = [
 const CalendarTabsSchema = z.enum(calendarTabs)
 
 export type CalendarTab = z.infer<typeof CalendarTabsSchema>
+
+// Events types
+
+const eventSchema = z.object({
+  summary: z.string(),
+  start: z.object({
+    dateTime: z.string()    
+  }),
+  end: z.object({
+    dateTime: z.string()    
+  }),
+  sentToCalendar: z.boolean()
+})
+
+export type Event = z.infer<typeof eventSchema>
+
+export type EventFormData = Pick<Event, 'summary'|'start'|'end'|'sentToCalendar'>
+
+const eventTypeSchema = z.object({
+  name: z.string(),
+  duration: z.number()
+})
+
+export const eventTypesSchema = z.array(eventSchema)
+
+export type EventTypes = z.infer<typeof eventTypesSchema>
+
+export type EventType = z.infer<typeof eventTypeSchema>
+
+export type EventTypeFormData = Pick<EventType, 'name'|'duration'>
