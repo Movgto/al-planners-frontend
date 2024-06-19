53b57edd2d76f0fe95051e29fb1f5f6bad92cd49
Gumaro Monroy <maromvz@gmail.com>
2024-06-18 21:42:35 -0600

    refactor: Improve AvailabilityTab component structure and styling

--- src/components/admins/calendar/AvailabilityTab.tsx
+++ src/components/admins/calendar/AvailabilityTab.tsx
@@ -26,59 +26,61 @@ const AvailabilityTab = () => {
     })
   }
   return (
-    <>
+    <div
+      className="flex flex-col p-4 gap-4"
+    >
       <p
-          className="text-slate-700 text-center"
-        >Establece las horas de disponibilidad para citas este dia</p>
+        className="text-slate-700 text-center"
+      >Establece las horas de disponibilidad para citas este dia</p>
 
-        <form
-          noValidate
-          className='px-2 flex flex-col gap-2'
-          onSubmit={handleSubmit(handleForm)}
+      <form
+        noValidate
+        className='flex flex-col gap-2'
+        onSubmit={handleSubmit(handleForm)}
+      >
+        <div
+          className='flex flex-col gap-2'
         >
-          <div
-            className='flex flex-col gap-2'
-          >
-            <label htmlFor="startTime">Inicio</label>
-            <input 
-              type="number"
-              id="startTime"
-              className='ring-1 ring-slate-300 rounded-sm p-1'
-              {...register('startTime',
-                {
-                  required: 'Este campo es requerido',
-                  valueAsNumber: true,
-                  min: 0,
-                  max: 24
-                }
-              )}
-            />
-          </div>
-          <div
-            className='flex flex-col gap-2'
-          >
-            <label htmlFor="endTime">Final</label>
-            <input 
-              type="number"
-              id="endTime"              
-              className='ring-1 ring-slate-300 rounded-sm p-1'
-              {...register('endTime',
-                {
-                  required: 'Este campo es requerido',
-                  valueAsNumber: true,
-                  min: 0,
-                  max: 24
-                }
-              )}
-            />
-          </div>
-          <input
-            type="submit"            
-            className="bg-cyan-500 text-white uppercase font-bold hover:cursor-pointer"
-            value="Abrir tiempo de disponibilidad"
+          <label htmlFor="startTime">Inicio</label>
+          <input 
+            type="number"
+            id="startTime"
+            className='ring-1 ring-slate-300 rounded-sm p-1'
+            {...register('startTime',
+              {
+                required: 'Este campo es requerido',
+                valueAsNumber: true,
+                min: 0,
+                max: 24
+              }
+            )}
           />
-        </form>
-    </>
+        </div>
+        <div
+          className='flex flex-col gap-2'
+        >
+          <label htmlFor="endTime">Final</label>
+          <input 
+            type="number"
+            id="endTime"              
+            className='ring-1 ring-slate-300 rounded-sm p-1'
+            {...register('endTime',
+              {
+                required: 'Este campo es requerido',
+                valueAsNumber: true,
+                min: 0,
+                max: 24
+              }
+            )}
+          />
+        </div>
+        <input
+          type="submit"            
+          className="bg-cyan-700 text-white uppercase font-bold hover:cursor-pointer hover:bg-cyan-500 p-2"
+          value="Abrir tiempo de disponibilidad"
+        />
+      </form>
+    </div>
   )
 }
 
