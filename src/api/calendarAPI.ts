bf9a22c3517126070659bfd0c44feb2583e93643
Gumaro Monroy <maromvz@gmail.com>
2024-06-18 21:42:10 -0600

    refactor: Update calendarAPI.ts to handle event type creation and deletion

--- src/api/calendarAPI.ts
+++ src/api/calendarAPI.ts
@@ -1,5 +1,5 @@
 import ax, {isAxiosError} from 'axios'
-import { EventFormData, EventTypeFormData, eventTypesSchema } from '../types'
+import { EventFormData, EventType, EventTypeFormData, eventListSchema, eventTypesSchema } from '../types'
 
 const {VITE_API_URL} = import.meta.env
 
@@ -39,7 +39,13 @@ export const getEvents = async () => {
 
   try {
     const {data} = await ax(url)
-     return data
+
+    const result = eventListSchema.safeParse(data)
+
+    console.log(result)
+
+    if (result.success) return result.data
+
   } catch (error) {
     handleAxiosError(error)
   }
@@ -72,10 +78,24 @@ export const createEvent = async (formData : EventFormData) => {
 }
 
 export const createEventType = async (formData : EventTypeFormData) => {
-  const url = VITE_API_URL + '/eventTypes'
+  const url = VITE_API_URL + '/api/eventTypes'
 
   try {
+
+    console.log(formData)
     const {data} = await ax.post(url, formData)
+
+    return data as string
+
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const deleteEventType = async (eventTypeId: EventType['_id']) => {
+  const url = VITE_API_URL + `/api/eventTypes/${eventTypeId}`
+  try {
+    const {data} = await ax.delete(url)
 
     return data
   } catch (error) {
