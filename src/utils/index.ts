7043fba0e25835a7c81cc4122e9679a89913000f
Gumaro Monroy <maromvz@gmail.com>
2024-06-18 21:43:08 -0600

    refactor: Add getDateInTimezone utility function for converting time to a specific timezone

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,9 @@
+import { DateTime } from "luxon"
+
 export const classes = (...classes: string[]) => {
   return classes.filter(Boolean).join(' ')
+}
+
+export const getDateInTimezone = (timeNumber: number) => {
+  return DateTime.fromISO(new Date(timeNumber).toISOString(), {zone: 'America/Mexico_City'}).toISO()!
 }
