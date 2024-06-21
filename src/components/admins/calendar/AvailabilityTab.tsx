781a17f62f965657d718a484f391df7389d71ef8
Gumaro Monroy <maromvz@gmail.com>
2024-06-21 12:20:34 -0600

    refactor: Update AvailabilityTab component to handle availability management in the calendar

--- src/components/admins/calendar/AvailabilityTab.tsx
+++ src/components/admins/calendar/AvailabilityTab.tsx
@@ -1,85 +1,83 @@
-import { CalendarDate, CalendarFormData } from '@/types/index'
-import { useQueryClient } from '@tanstack/react-query'
-import { useForm } from 'react-hook-form'
+import { deleteAvailableTime, getAvailableTimes } from '@/api/calendarAPI'
+import { useSelectedDate } from '@/hooks/index'
+import { Availability } from '@/types/index'
+import { formatHour } from '@/utils/index'
+import { TrashIcon } from '@heroicons/react/24/outline'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 
 const AvailabilityTab = () => {
 
+  const selectedDate = useSelectedDate()
+
+  const navigate = useNavigate()
+
   const queryClient = useQueryClient()
 
-  const defaultValues : CalendarFormData = {
-    startTime: -1,
-    endTime: -1
+  const {data, isLoading, isError, error} = useQuery({
+    queryKey: ['availabilityTimes', selectedDate.toISOString()],
+    queryFn: () => getAvailableTimes(selectedDate.toISOString())
+  })
+
+  const { mutate } = useMutation({
+    mutationFn: deleteAvailableTime,
+    onError: error => toast.error(error.message),
+    onSuccess: data => {
+      queryClient.invalidateQueries({queryKey: ['availabilityTimes', selectedDate.toISOString()]})
+      toast.success(data)
+    }
+  })
+
+  const handleClick = () => {
+    navigate('?createAvailability=true')
   }
 
-  const {register, handleSubmit} = useForm({defaultValues})
-
-  const handleForm = (formData: CalendarFormData) => {
-
-    const date = queryClient.getQueryData<CalendarDate>(['selectedDate'])
-
-    if (!date) return
-
-    console.log({
-      startTime: formData.startTime,
-      endTime: formData.endTime,
-      date
-    })
+  const handleDelete = (availabilityId: Availability['_id']) => {
+    mutate(availabilityId)
   }
-  return (
+
+  if (isLoading) return 'Obteniendo tiempos de disponibilidad'
+
+  if (isError) return error.message
+
+  if (data) return (
     <div
-      className="flex flex-col p-4 gap-4"
+      className='min-w-80 p-4 h-full flex flex-col gap-4'
     >
-      <p
-        className="text-slate-700 text-center"
-      >Establece las horas de disponibilidad para citas este dia</p>
-
-      <form
-        noValidate
-        className='flex flex-col gap-2'
-        onSubmit={handleSubmit(handleForm)}
+      <ul
+        className="flex gap-2 flex-wrap"
       >
-        <div
-          className='flex flex-col gap-2'
+        <li className="flex-1">
+          <button
+            type="button"
+            onClick={handleClick}
+            className="bg-rose-700 py-2 text-white font-bold uppercase w-full hover:bg-rose-500 text-nowrap px-2"
+          >Abrir Disponibilidad</button>
+        </li>
+      </ul>
+
+      {data.length ? (
+        <ul
+          className="flex flex-col gap-2"
         >
-          <label htmlFor="startTime">Inicio</label>
-          <input 
-            type="number"
-            id="startTime"
-            className='ring-1 ring-slate-300 rounded-sm p-1'
-            {...register('startTime',
-              {
-                required: 'Este campo es requerido',
-                valueAsNumber: true,
-                min: 0,
-                max: 24
-              }
-            )}
-          />
-        </div>
-        <div
-          className='flex flex-col gap-2'
-        >
-          <label htmlFor="endTime">Final</label>
-          <input 
-            type="number"
-            id="endTime"              
-            className='ring-1 ring-slate-300 rounded-sm p-1'
-            {...register('endTime',
-              {
-                required: 'Este campo es requerido',
-                valueAsNumber: true,
-                min: 0,
-                max: 24
-              }
-            )}
-          />
-        </div>
-        <input
-          type="submit"            
-          className="bg-cyan-700 text-white uppercase font-bold hover:cursor-pointer hover:bg-cyan-500 p-2"
-          value="Abrir tiempo de disponibilidad"
-        />
-      </form>
+          {data.map(a => (
+            <li
+              className="bg-white text-center text-slate-600
+                          font-bold p-2 flex justify-between items-center"
+            >
+              <p>{formatHour(a.startTime)} - {formatHour(a.endTime)}</p>
+              
+              <i
+                className='text-red-600 hover:cursor-pointer hover:text-red-300'
+              ><TrashIcon height='24px' width='24px' onClick={() => handleDelete(a._id)} /></i>              
+            </li>
+          ))}
+        </ul>
+      )
+      : (
+        <p>No hay tiempos de disponibilidad en esta fecha</p>
+      )}
     </div>
   )
 }
