246a0a42dddc3cade0084427ae6cc7d607e18149
Gumaro Monroy <maromvz@gmail.com>
2024-06-21 12:20:13 -0600

    refactor: Update calendarAPI.ts to add new functions for event and availability management

--- src/api/calendarAPI.ts
+++ src/api/calendarAPI.ts
@@ -1,13 +1,8 @@
-import ax, {isAxiosError} from 'axios'
-import { EventFormData, EventType, EventTypeFormData, eventListSchema, eventTypesSchema } from '../types'
+import ax from 'axios'
+import {handleAxiosError} from '@/utils/api'
+import { Availability, AvailabilityFormData, Event, EventFormData, EventType, EventTypeFormData, availabilityTimesSchema, eventListSchema, eventSchema, eventTypesSchema } from '../types'
 
 const {VITE_API_URL} = import.meta.env
-
-const handleAxiosError = (error: unknown) => {
-  if (isAxiosError(error) && error.response) {
-    throw new Error(error.response.data.error)
-  }
-}
 
 export const getAuthURL = async () => {
   const url = VITE_API_URL + '/api/googleapi/auth-url'
@@ -46,6 +41,32 @@ export const getEvents = async () => {
 
     if (result.success) return result.data
 
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const getEvent = async (eventId: Event['_id']) => {
+  
+  const url = VITE_API_URL + `/api/events/${eventId}`
+  try {
+    const {data} = await ax(url)
+
+    const result = eventSchema.safeParse(data)
+
+    if (result.success) return result.data
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const syncEvents = async (code: string) => {
+  const url = VITE_API_URL + `/api/googleapi/syncEvents`
+  try {
+    const {data} = await ax.post(url, {code})
+  
+    return data
+    
   } catch (error) {
     handleAxiosError(error)
   }
@@ -94,6 +115,64 @@ export const createEventType = async (formData : EventTypeFormData) => {
 
 export const deleteEventType = async (eventTypeId: EventType['_id']) => {
   const url = VITE_API_URL + `/api/eventTypes/${eventTypeId}`
+  try {
+    const {data} = await ax.delete(url)
+
+    return data
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+type DeleteEventParams = {
+  code: string,
+  eventId: string
+}
+
+export const deleteEvent = async ({code, eventId} : DeleteEventParams) => {
+  const url = VITE_API_URL + `/api/googleapi/events/${eventId}`
+
+  try {
+    const {data} = await ax.delete(url, {data: {code}})
+
+    return data
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+// Availability times
+
+export const createAvailableTime = async (formData: AvailabilityFormData) => {
+  const url = VITE_API_URL + '/api/availability'
+
+  try {
+    const {data} = await ax.post(url, formData)
+
+    return data
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const getAvailableTimes = async (isoDate: string) => {
+  const url = VITE_API_URL + `/api/availability/${isoDate}`
+
+  try {
+    const {data} = await ax(url)
+
+    const result = availabilityTimesSchema.safeParse(data)
+
+    if (result.success) return result.data
+    
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const deleteAvailableTime = async (availabilityId: Availability['_id']) => {
+  const url = VITE_API_URL + `/api/availability/${availabilityId}`
+
   try {
     const {data} = await ax.delete(url)
 
