b2cac6df4d48e8a8ccdeeb227f27493614656168
Gumaro Monroy <maromvz@gmail.com>
2024-06-28 20:57:16 -0600

    feat: Add ScheduleView component for displaying user schedules

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,9 @@ import HomeView from "@/views/admins/HomeView"
 import AdminsLayout from "@/layouts/admins/AdminsLayout"
 import CalendarView from "@/views/admins/CalendarView"
 import UsersHomeView from "@/views/users/UsersHomeView"
+import ScheduleView from "./views/users/ScheduleView"
+import AdminsAuthLayout from "./layouts/admins/AdminsAuthLayout"
+import AdminsLoginView from "./views/admins/auth/AdminsLoginView"
 
 const Router = () => {
   return (
@@ -11,10 +14,14 @@ const Router = () => {
       <Routes>
         <Route path='/' element={<AppLayout />}>
           <Route index element={<UsersHomeView />} />
+          <Route path="schedule" element={<ScheduleView />} />
         </Route>
         <Route path='admins' element={<AdminsLayout />}>
           <Route index element={<HomeView />} />
-          <Route path='calendar' element={<CalendarView />} />
+          <Route path='calendar' element={<CalendarView />} />          
+        </Route>
+        <Route path='auth/admins' element={<AdminsAuthLayout />}>
+          <Route path="login" element={<AdminsLoginView />} />
         </Route>
       </Routes>
     </BrowserRouter>
