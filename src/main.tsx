4c4421f00c7287444cca53e268859d3a60453a57
Gumaro Monroy <maromvz@gmail.com>
2024-06-28 20:57:09 -0600

    refactor: Add ParallaxProvider for scroll parallax effect

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import './index.css'
 import Router from './Router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ParallaxProvider } from 'react-scroll-parallax'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Router />
-      <ReactQueryDevtools />
+      <ParallaxProvider>
+        <Router />
+        <ReactQueryDevtools />
+      </ParallaxProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
