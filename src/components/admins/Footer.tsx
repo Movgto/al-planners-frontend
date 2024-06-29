8225f0e487734a754eeedde2eb17fe9e8a614fdc
Gumaro Monroy <maromvz@gmail.com>
2024-06-28 20:57:52 -0600

    refactor: Add padding to footer for better spacing

--- src/components/admins/Footer.tsx
+++ src/components/admins/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="text-sm text-slate-500 font-semibold">
+    <footer className="text-sm text-slate-500 font-semibold p-6">
       <i>AL Planners © {new Date().getFullYear()}</i>
     </footer>
   )
