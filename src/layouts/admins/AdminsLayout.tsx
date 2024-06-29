82ecb0c7398a68b86116e4ea85e546ffbb7b6936
Gumaro Monroy <maromvz@gmail.com>
2024-06-28 20:59:46 -0600

    feat: Add lazy loading for images in AdminsLayout

--- src/layouts/admins/AdminsLayout.tsx
+++ src/layouts/admins/AdminsLayout.tsx
@@ -1,11 +1,23 @@
 import Footer from "@/components/admins/Footer"
 import Header from "@/components/admins/Header"
-import { Outlet } from "react-router-dom"
+import useAdmin from "@/hooks/useAdmin"
+import { Navigate, Outlet } from "react-router-dom"
 import {ToastContainer} from 'react-toastify'
 import 'react-toastify/ReactToastify.css'
 
 const AdminsLayout = () => {
-  return (
+
+  const {data, isLoading, isError} = useAdmin()
+
+  if (isLoading) {
+    return null
+  }
+
+  if (isError) {
+    return <Navigate to='/auth/admins/login' />
+  }
+
+  if (data) return (
     <>
       <Header />
       <Outlet />
