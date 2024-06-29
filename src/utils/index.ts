8d16089a5fc8d916ee84288467b8e303fcb4ebce
Gumaro Monroy <maromvz@gmail.com>
2024-06-28 22:45:28 -0600

    feat: Add dateInTimezone utility function to handle timezones in Calendar component

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -8,6 +8,10 @@ export const getDateInTimezone = (timeNumber: number) => {
   return DateTime.fromISO(new Date(timeNumber).toISOString(), {zone: 'America/Mexico_City'}).toISO()!
 }
 
+export const dateInTimezone = (date: Date) => {
+  return new Date(date.toLocaleString('en-US', {timeZone: 'America/Mexico_City'}))
+}
+
 export const formatHour = (isoDate: string) => {
   const date = new Date(isoDate)
 
