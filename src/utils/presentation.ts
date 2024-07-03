dc07fc9a584dd89d438291b924bdcdc1630fada5
Gumaro Monroy <maromvz@gmail.com>
2024-07-03 13:06:15 -0600

    feat: Add collage images to presentation utils

---
+++ src/utils/presentation.ts
@@ -0,0 +1,26 @@
+export const collageData = [
+  {
+    url: '/collage_01.jpg'
+  },
+  {
+    url: '/collage_02.jpg'
+  },
+  {
+    url: '/collage_03.jpg'
+  },
+  {
+    url: '/collage_04.jpg'
+  },
+  {
+    url: '/collage_05.jpg'
+  },
+  {
+    url: '/collage_06.jpg'
+  },
+  {
+    url: '/collage_07.jpg'
+  },
+  {
+    url: '/collage_08.jpg'
+  },
+]
