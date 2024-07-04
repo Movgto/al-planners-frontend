1d86bac43032846162bde029503d5b2142dda9f8
Gumaro Monroy <maromvz@gmail.com>
2024-07-03 19:50:52 -0600

    refactor: Optimize image loading in UsersHomeView

--- src/views/users/UsersHomeView.tsx
+++ src/views/users/UsersHomeView.tsx
@@ -1,9 +1,11 @@
 import ShowOnScreen from '@/components/animated/ShowOnScreen'
 import '@/styles/landpage.css'
 import { Link } from 'react-router-dom'
-import { ParallaxBanner } from 'react-scroll-parallax'
 import ALLogoBlack from '@/images/logos/al_logo_black.png'
-import { collageData } from '@/utils/presentation'
+import ALCircleBlack from '@/images/logos/al_logo_circle_black.png'
+import { collageData, sliderContent } from '@/utils/presentation.tsx'
+import SlideHorizontal from '@/components/animated/SlideHorizontal'
+import Slider from '@/components/animated/Slider'
 
 const UsersHomeView = () => {
 
@@ -14,7 +16,7 @@ const UsersHomeView = () => {
          bg-white my-16 w-full overflow-y-clip"
       >
         <div
-          className='absolute top-0 left-0 grid grid-cols-1 grid-flow-row opacity-50'
+          className='absolute top-0 left-0 hidden lg:grid grid-cols-1 grid-flow-row opacity-50'
         >
           {collageData.map((data, index) => {
 
@@ -40,7 +42,7 @@ const UsersHomeView = () => {
           className='absolute top-0 right-0 hidden lg:grid grid-cols-1 grid-flow-row opacity-50 '
         >
           {collageData.map((data, index) => {
-            if (index < 4) return null            
+            if (index < 4) return null
 
             return (
               <ShowOnScreen
@@ -60,7 +62,7 @@ const UsersHomeView = () => {
         </div>
         <div
           className="max-w-5xl flex flex-col px-2 py-6
-          lg:flex-row lg:items-center lg:p-6 gap-6 justify-center border-t border-black my-4"
+          lg:flex-row lg:items-center lg:p-6 gap-6 justify-center border-t border-zinc-800 my-4"
         >
           <ShowOnScreen
             extraClasses='flex flex-col font-lora text-justify text-xl font-thin flex-1 gap-6 bg-white/60 p-4'
@@ -114,119 +116,85 @@ const UsersHomeView = () => {
           </ShowOnScreen>
         </div>
       </div>
-      <div
-        className='grid grid-rows-3 lg:grid-rows-1 grid-cols-4 lg:grid-cols-8 mb-0 lg:mb-10'
+
+      <SlideHorizontal
+        extraClasses='flex w-full max-w-5xl overflow-x-auto transition ease mb-10'
       >
-        {collageData.map((data, index) => (
+        <div className='flex gap-10'>
+          {collageData.map((data, index) => (
+            <ShowOnScreen
+              reappear
+              threshold={0.2}
+              transitionInClasses={`duration-500 translate-y-0 opacity-100 delay-[${100 + 200 * index}ms] ease-out]`}
+              transitionOutClasses='duration-200 -translate-y-2 scale-80 opacity-60 ease-in'
+              extraClasses="flex-shrink-0 bg-cover bg-no-repeat bg-center w-60 h-60"
+            >
+              <img
+                alt={`collage 2 photo ${index + 1}`}
+                src={data.url}
+                className='object-cover object-center w-full h-full'
+              />
+            </ShowOnScreen>
+          ))}
+        </div>
+      </SlideHorizontal>
+      <section
+        className="overflow-y-auto w-full"
+      >
+        <Slider
+          sliderContent={sliderContent}
+        />
+      </section>
+
+      <ShowOnScreen
+        extraClasses='w-full flex justify-center items-center min-h-screen'
+        transitionInClasses='duration-500 opacity-100 scale-x-100 ease-out'
+        transitionOutClasses='duration-200 opacity-0 scale-x-50 ease-in'
+      >
+        <div className='w-full bg-white border-zinc-800 border-y-2
+                        py-20 flex flex-col items-center gap-10 z-10'
+        >
           <ShowOnScreen
-            transitionInClasses={`duration-500 translate-y-0 opacity-100 delay-[${100 + 200 * index}ms] ease-out]`}
-            transitionOutClasses='duration-200 -translate-y-2 opacity-0'
-            extraClasses="bg-cover bg-no-repeat bg-center w-60 h-60"
+            transitionInClasses='translate-y-0 opacity-100 ease-out duration-500 delay-[300ms]'
+            transitionOutClasses='-translate-y-4 opacity-0 ease-in duration-200'
           >
             <img
-              alt={`collage 2 photo ${index + 1}`}
-              src={data.url}
-              className='object-cover object-center w-full h-full'
+              alt="schedule al logo"
+              src={ALCircleBlack}
+              className='object-contain w-40'
             />
           </ShowOnScreen>
-        ))}
-      </div>
-      <section
-        className="overflow-y-auto"
-      >
-        <div className='flex flex-col items-center gap-32'>
           <ShowOnScreen
-            extraClasses="flex flex-col lg:flex-row gap-12 justify-between bg-black/40 rounded-2xl p-10 max-w-6xl shadow-2xl"
-            threshold={0.5}
-            reappear
+            extraClasses='flex flex-nowrap overflow-x-clip justify-center'
+            transitionInClasses='duration-700 w-full ease-out opacity-100 delay-[1200ms]'
+            transitionOutClasses='duration-200 opacity-0 w-0 ease-in'
           >
-            <div
-              className='flex-1 bg-[url("/presentation_02.jfif")] min-h-[24rem] bg-contain bg-no-repeat bg-center'
-            ></div>
-            <p
-              className='font-lora text-white text-xl font-thin flex-1'
-            >
-              <span className='font-bold'>¡Más de 5 años haciendo sueños realidad!</span>
-              <br />
-              <br />
-              Llevamos más de 5 años en el mercado y decenas de eventos nos respaldan como uno de los proveedores
-              de coordinación más solicitados de Monterrey y área metropolitana.
-
-              Contamos con una amplia cartera de proveedores confiables y capaces de ofrecerte cualquier detalle
-              que necesites para ese gran día.
-            </p>
+            <h2 className='font-lora text-2xl text-justify text-zinc-800 p-2'>Si quieres saber más sobre la boda de tus sueños...</h2>
+          </ShowOnScreen>
+          <ShowOnScreen
+            extraClasses='flex flex-nowrap overflow-x-clip justify-center'
+            transitionInClasses='duration-700 w-full ease-out opacity-100 delay-[500ms]'
+            transitionOutClasses='duration-200 opacity-0 w-0 ease-in'
+          >
+            <hr className='border border-[#d0c383] w-1/2' />
+          </ShowOnScreen>
+          <ShowOnScreen
+            extraClasses='flex flex-nowrap overflow-x-clip justify-center'
+            transitionInClasses='duration-700 scale-100 ease-out opacity-100 delay-[2100ms]'
+            transitionOutClasses='duration-200 scale-50 opacity-0 ease-in'
+          >
+            <Link
+              to="/schedule"
+              className='font-lora bg-zinc-600 hover:cursor-pointer hover:bg-white
+                      transition hover:text-zinc-800 py-2 px-4 text-white font-bold text-2xl'
+            >Agendar Cita</Link>
           </ShowOnScreen>
 
-          <ShowOnScreen
-            extraClasses="flex flex-col lg:flex-row gap-12 justify-between bg-black/40 rounded-2xl p-10 max-w-6xl shadow-2xl"
-            threshold={0.5}
-            reappear
-          >
-            <div
-              className='flex-1 bg-[url("/presentation_03.jfif")] min-h-[24rem] bg-contain bg-no-repeat bg-center'
-            ></div>
-            <p
-              className='font-lora text-white text-xl font-thin flex-1'
-            >
-              <span className='font-bold'>¡No todo son bodas!</span>
-              <br />
-              <br />
-              En el transcurso de estos años hemos crecido de manera que ya no solo hacemos bodas.<br /><br />
-              Eventos empresariales, posadas, XV años, Baby Shower o el evento que necesites, contáctanos
-              y averigua de qué forma podemos ayudarte.
-            </p>
-          </ShowOnScreen>
-
-          <ShowOnScreen
-            extraClasses="flex flex-col lg:flex-row gap-12 justify-between bg-black/40 rounded-2xl p-10 max-w-6xl w-full shadow-2xl"
-            threshold={0.5}
-            reappear
-          >
-            <div
-              className='flex flex-col flex-1 gap-4 font-lora text-white text-xl font-thin'
-            >
-              <p>
-                <span className='font-bold'>Servicios</span>
-                <br />
-                <br />
-                Algunos de los servicios con los que contamos podemos mencionar:
-              </p>
-              <ul className='list-disc'>
-                <li>Diseño de tu evento</li>
-                <li>Recomendaciones/Asesoría de decoración</li>
-                <li>Asesoría ilimitada</li>
-                <li>Logística del evento</li>
-                <li>Cartera de proveedores</li>
-                <li>Supervisión del montaje</li>
-                <li>Conteo de asistencia y platillos</li>
-                <li>Confirmación de invitados</li>
-                <li>Coordinación de ceremonia civil</li>
-                <li>Coordinación de ceremonia religiosa</li>
-                <li>Coordinación de despedida de soltera</li>
-                <li>Recolección y entrega de ramos</li>
-                <li>Hostess</li>
-              </ul>
-            </div>
-            <div
-              className='flex-1 bg-[url("/presentation_04.jfif")] min-h-[24rem] bg-contain bg-no-repeat bg-center'
-            ></div>
-          </ShowOnScreen>
+          
+          
         </div>
-      </section>
-      <ParallaxBanner
-        layers={[
-          {
-            image: "/schedulelink_bg.jpg",
-            speed: 50
-          }
-        ]}
-        className='min-h-screen my-10 flex justify-center items-center'
-      >
-        <div className='inset-0 flex flex-col items-center gap-10 z-10'>
-          <h2 className='font-lora text-2xl text-white bg-black/40 p-2'>Si quieres saber más sobre la boda de tus sueños...</h2>
-          <Link to="/schedule" className='bg-zinc-600 hover:cursor-pointer hover:bg-white hover:text-zinc-800 py-2 px-4 text-white font-bold text-2xl'>Agendar Cita</Link>
-        </div>
-      </ParallaxBanner>
+      </ShowOnScreen>
+
       <section
         id="contact"
         className='text-xl px-4 py-6'
