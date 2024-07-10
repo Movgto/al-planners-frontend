039d2c01c9b3ccd8146fe023a380e23d528cfceb
Gumaro Monroy <maromvz@gmail.com>
2024-07-09 21:23:24 -0600

    Refactor AppointmenstList component to improve code readability and maintainability

--- src/components/admins/calendar/AppointmenstList.tsx
+++ src/components/admins/calendar/AppointmenstList.tsx
@@ -1,7 +1,7 @@
 import { Event, EventList } from "@/types/index"
 import { useEventsFromDate, useGApiCode } from "@/hooks/index"
 import { useNavigate } from "react-router-dom"
-import {formatHour} from '@/utils/index'
+import { formatHour } from '@/utils/index'
 import { TrashIcon } from "@heroicons/react/24/outline"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
 import { deleteEvent } from "@/api/calendarAPI"
@@ -12,7 +12,7 @@ type AppointmenstListProps = {
   isoDate: string
 }
 
-const AppointmenstList = ({eventList, isoDate} : AppointmenstListProps) => {
+const AppointmenstList = ({ eventList, isoDate }: AppointmenstListProps) => {
 
   const navigate = useNavigate()
 
@@ -37,13 +37,13 @@ const AppointmenstList = ({eventList, isoDate} : AppointmenstListProps) => {
       toast.error(error.message)
     },
     onSuccess: data => {
-      queryClient.invalidateQueries({queryKey: ['calendarEvents']})
+      queryClient.invalidateQueries({ queryKey: ['calendarEvents'] })
       toast.success(data)
     }
   })
 
   const handleDelete = (eventId: string) => {
-    mutate({code: code || 'asdasdasd', eventId})
+    mutate({ code: code || 'asdasdasd', eventId })
   }
 
   return (
@@ -53,22 +53,32 @@ const AppointmenstList = ({eventList, isoDate} : AppointmenstListProps) => {
       {eventsFromDate.length ? eventsFromDate.map(e => (
         <li
           className={classes(e.sentToCalendar ? 'bg-sky-200' : 'bg-red-200',
-            "rounded-md border border-slate-300 font-semibold",
-            "flex justify-between p-2 items-center hover:cursor-pointer hover:border-slate-800")}
-          onClick={() => handleClick(e._id)}
+            "rounded-md border border-slate-400 font-semibold",
+            "flex relative pr-14 group hover:border-slate-600 hover:cursor-pointer")}
         >
-          <p>{e.summary}</p>
-
-          <div          
-            className="flex gap-2 text-sm text-slate-800 font-semibold"
+          <div
+            className="flex w-full justify-between items-center p-2 z-10"
+            onClick={() => handleClick(e._id)}
           >
-            <p>{formatHour(e.start.dateTime)}</p>{' - '}
-            <p>{formatHour(e.end.dateTime)}</p>
+            <p>{e.summary}</p>
+
+            <div
+              className="flex gap-2 text-sm text-slate-800 font-semibold"
+            >
+              <p>{formatHour(e.start.dateTime)}</p>{' - '}
+              <p>{formatHour(e.end.dateTime)}</p>
+            </div>
           </div>
-          <i
-            className="text-red-600 hover:text-red-300 hover:cursor-pointer z-[5]"
-            onClick={() => handleDelete(e._id)}
-          ><TrashIcon width="24px" height="24px" /></i>
+
+          <div
+            className="absolute inset-0 flex justify-end items-center pr-5"
+          >
+            <i
+              className="text-red-600
+                    hover:text-red-300 hover:cursor-pointer"
+              onClick={() => handleDelete(e._id)}
+            ><TrashIcon width="24px" height="24px" /></i>
+          </div>
         </li>
       )) : (
         <li
