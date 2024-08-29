4ec55014fd6c5e57b452c57899cb0a6d030418f0
Gumaro Monroy <maromvz@gmail.com>
2024-08-29 15:18:06 -0600

    feat: Add getAdmins and getAdminPreferences functions to authAPI

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -1,4 +1,4 @@
-import { LoginFormData, SignUpFormData, userShortSchema } from "@/types/auth"
+import { LoginFormData, SignUpFormData, adminPreferencesSchema, adminShortSchema, adminsList } from "@/types/auth"
 import { handleAxiosError } from "@/utils/api"
 import api from "@/config/axios"
 
@@ -6,7 +6,7 @@ export const adminSignUp = async (formData: SignUpFormData) => {
   const url = '/auth/admins/signup'
 
   try {
-    const {data} = await api.post(url, formData)
+    const { data } = await api.post(url, formData)
 
     return data
   } catch (error) {
@@ -18,7 +18,7 @@ export const adminLogin = async (formData: LoginFormData) => {
   const url = '/auth/admins/login'
 
   try {
-    const {data} = await api.post(url, formData)
+    const { data } = await api.post(url, formData)
 
     localStorage.setItem('ALPLANNERS_AUTH_TOKEN', data)
 
@@ -32,12 +32,41 @@ export const getAdmin = async () => {
   const url = '/auth/admins'
 
   try {
-    const {data} = await api(url)
+    const { data } = await api(url)
 
-    const result = userShortSchema.safeParse(data)
+    const result = adminShortSchema.safeParse(data)
 
     if (result.success) return result.data
 
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const getAdmins = async () => {
+  const url = '/auth/adminsList'
+
+  try {
+    const { data } = await api(url)
+    
+    const result = adminsList.safeParse(data)
+
+    if (result.success) return result.data    
+
+  } catch (error) {
+    handleAxiosError(error)
+  }
+}
+
+export const getAdminPreferences = async () => {
+  const url = '/auth/admins/preferences'
+
+  try {
+    const {data} = await api(url)
+
+    const result = adminPreferencesSchema.safeParse(data)
+
+    if (result.success) return result.data
   } catch (error) {
     handleAxiosError(error)
   }
