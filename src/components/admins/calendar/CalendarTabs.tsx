c1a2df44b5cb41af1246ad9e3c58a0a690d54307
Gumaro Monroy <maromvz@gmail.com>
2024-08-29 15:18:30 -0600

    feat: Add PreferencesTab component to CalendarTabs

--- src/components/admins/calendar/CalendarTabs.tsx
+++ src/components/admins/calendar/CalendarTabs.tsx
@@ -1,6 +1,7 @@
 import { CalendarTab } from "@/types/index"
 import AvailabilityTab from "./AvailabilityTab"
 import AppointmentsTab from "./AppointmentsTab"
+import PreferencesTab from "./PreferencesTab"
 
 type CalendarTabsProps = {
   tab: CalendarTab,
@@ -10,6 +11,8 @@ const CalendarTabs = ({tab} : CalendarTabsProps) => {
   if(tab === 'Citas') return <AppointmentsTab />
 
   if(tab === 'Disponibilidad') return <AvailabilityTab />
+
+  if(tab === 'Preferencias') return <PreferencesTab />
 
   return null
 }
